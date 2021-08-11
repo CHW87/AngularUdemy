8df8079a69846257954c0c6a6e8598c46d4588fb
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-08-11 17:17:38 +0300

    Practice3 directions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
-  styles:[`
-  h3 {
-    color:dodgerblue;
-  }`]
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }`]
 })
 export class AppComponent {
+  showSecret = false;
+  logArr = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // @ts-ignore
+    // this.logArr.push(this.logArr.length +1);
+    this.logArr.push(new Date());
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,26 +7,33 @@ import {Component, OnInit} from '@angular/core';
   // template: `
   //   <app-server></app-server>
   //   <app-server></app-server>`,
-  templateUrl:'./servers.component.html',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
+
   constructor() {
-    setTimeout(() =>{
+    setTimeout(() => {
       this.allowNewServer = true;
-    },2000);
+    }, 2000);
   }
 
   ngOnInit() {
   }
-    onCreateServer(){
-      this.serverCreationStatus = 'Server was created!Name is' + this.serverName;
-    }
-    onUpdateServerName(event: Event){
-      this.serverName = (<HTMLInputElement>event.target).value;
-    }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created!Name is' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }
